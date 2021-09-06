62f2d31d472db6f4c4a480ec068d30a40f8ab652
ParkGunWook <saiver852@gmail.com>
2021-09-06 14:07:16 +0900

    기초 세팅 완료

---
+++ src/test/java/com/example/karaokesearch/KaraokeSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.karaokesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaraokeSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
