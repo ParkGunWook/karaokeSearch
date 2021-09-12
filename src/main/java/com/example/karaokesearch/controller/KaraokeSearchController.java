4f7bd23ec3dea8afb2e0c19619ff4a1e159dd632
ParkGunWook <saiver852@gmail.com>
2021-09-12 20:33:55 +0900

    CrossOrigin Annotation added.

--- src/main/java/com/example/karaokesearch/controller/KaraokeSearchController.java
+++ src/main/java/com/example/karaokesearch/controller/KaraokeSearchController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("api/search")
 @Api(tags = "통합 검색수행하는 api입니다.")
